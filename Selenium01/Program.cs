06a92d4a7b9c966fa7cd14c5181b3ecacdc386a2
Masihur Maruf <marufrahman1349@gmail.com>
2017-03-25 23:01:08 -0400

    basic project

---
+++ Selenium01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium01
+{
+	// Need System.core package to be adde tin the reference to work... Don't know the reason
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			/// Need the chromedriver in the path secified with latest version of mac.
+			IWebDriver driver = new ChromeDriver(@"/Users/masihur/MyDevelopment/Selenium/C#/Selenium01/Selenium01/");
+			driver.Navigate().GoToUrl("http://www.google.com");
+
+			IWebElement element = driver.FindElement(By.Name("q"));
+			element.SendKeys("Masihur's Blog \n");
+			//element.SendKeys(Keys.Enter);
+			driver.Close();
+		}
+	}
+	
+}
