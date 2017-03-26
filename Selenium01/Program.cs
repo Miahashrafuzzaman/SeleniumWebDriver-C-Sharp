4ffdfbad5563f7efcaf078705868a3d6126b5a84
Masihur Maruf <marufrahman1349@gmail.com>
2017-03-26 00:20:52 -0400

    add NUnit

--- Selenium01/Program.cs
+++ Selenium01/Program.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace Selenium01
 {
-	// Need System.core package to be adde tin the reference to work... Don't know the reason
+	// Faced the issue to build the project once ... not sure how it was resolved... Need to investigate further...
 	class MainClass
 	{
+		IWebDriver driver = new ChromeDriver(@"/Users/masihur/MyDevelopment/Selenium/C#/Selenium01/Selenium01/");
+
 		public static void Main(string[] args)
 		{
-			/// Need the chromedriver in the path secified with latest version of mac.
-			IWebDriver driver = new ChromeDriver(@"/Users/masihur/MyDevelopment/Selenium/C#/Selenium01/Selenium01/");
-			driver.Navigate().GoToUrl("http://www.google.com");
 
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			driver.Navigate().GoToUrl("http://www.google.com");
+		}
+		[Test]
+		public void ExecuteTest()
+		{
 			IWebElement element = driver.FindElement(By.Name("q"));
 			element.SendKeys("Masihur's Blog \n");
-			//element.SendKeys(Keys.Enter);
+		}
+		[TearDown]
+		public void TearDown()
+		{
 			driver.Close();
 		}
+
 	}
+
+
 	
 }
